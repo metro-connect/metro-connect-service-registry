bbe82ca8cc3a3694688fe611d62e5b055057d129
metro-connect <sameerinamdar.sae@gmail.com>
2024-08-05 01:24:21 +0530

    No One Care

---
+++ src/main/java/com/metro/connect/MetroConnectServiceRegistryApplication.java
@@ -0,0 +1,15 @@
+package com.metro.connect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class MetroConnectServiceRegistryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MetroConnectServiceRegistryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/metro/connect/MetroConnectServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.metro.connect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MetroConnectServiceRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
